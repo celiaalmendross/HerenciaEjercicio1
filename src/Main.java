cd18f032becedda824b95a28929f622f7df8e8c1
Celia <celia.almendros@alu.uclm.es>
2023-03-10 12:11:23 +0100

    Creacion clase conserje

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Empleado empleado1= new Empleado();
+        System.out.println(+empleado1.getVacaciones());
+        Empleado abogado= new Abogado();
+        System.out.println(abogado.getVacaciones());
+
 
     }
 }
